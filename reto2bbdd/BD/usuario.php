6870c94161a7fe7b88447de3e9bbe7a45aa15e94
aritz <aritz.martinezh@gmail.com>
2019-11-19 10:52:18 +0100

    limpiado

--- reto2bbdd/BD/usuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-function insertUsuario($dbh,$username,$pass){
-    $data = array('username' => $username, 'pwd' => $pass);
-
-    $stmt = $dbh->prepare("INSERT INTO Usuarios (nomUsuario, password) VALUES (:username, :pwd)");
-    $stmt->setFetchMode(PDO::FETCH_OBJ);
-    $stmt->execute($data);
-}
-?>
